464dce081d860baa30809ad3cdee7ecd2de5b152
Dejan Muhamedagic <dejan@hello-penguin.com>
2011-08-29 18:00:41 +0200

    Low: Shell: let the pager decide how to handle output smaller than terminal
    Instead of trying to calculate the size of the output, which may
    not be trivial, better let the pager deal with it. For instance,
    less(1) can be configured to exit immediately on a
    less-than-screenful of input (-F). IIRC, more(1) does that
    automatically.

--- modules/utils.py
+++ modules/utils.py
@@ -524,10 +524,7 @@ def page_string(s):
     'Write string through a pager.'
     if not s:
         return
-    w,h = get_winsize()
-    if s.count('\n') < h:
-        print s
-    elif not user_prefs.pager or not sys.stdout.isatty() or options.batch:
+    if not user_prefs.pager or not sys.stdout.isatty() or options.batch:
         print s
     else:
         opts = ""
