f52b8909fbe765b414fc38fb03a505e678e192dc
Kristoffer Grönlund <kgronlund@suse.com>
2013-10-04 09:06:00 +0200

    Dev: Clean up unused definitions from autotools chain
    * Remove libtool references from autogen.sh
    * Remove UUID_FILE from configure.ac
    * Remove STONITH_PLUGIN_DIR from configure.ac

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@
 #
 #	bootstrap: set up the project and get it ready to make
 #
-#	Basically, we run autoconf, automake and libtool in the
+#	Basically, we run autoconf and automake in the
 #	right way to get things set up for this environment.
 #
 #	We also look and see if those tools are installed, and
@@ -46,17 +46,17 @@ testProgram()
   fi
 
   # The GNU standard is --version
-  if 
+  if
       $cmd --version </dev/null >/dev/null 2>&1
   then
-      return 0 
+      return 0
   fi
 
   # Maybe it suppports -V instead
-  if 
+  if
       $cmd -V </dev/null >/dev/null 2>&1
   then
-      return 0 
+      return 0
   fi
 
   # Nope, the program seems broken
@@ -80,9 +80,9 @@ do
   fi
 done
 
-for command in automake14 automake-1.4 automake15 automake-1.5 automake17 automake-1.7 automake19 automake-1.9 automake-1.11 automake 
+for command in automake14 automake-1.4 automake15 automake-1.5 automake17 automake-1.7 automake19 automake-1.9 automake-1.11 automake
 do
-  if 
+  if
       testProgram $command
   then
     : OK $pkg is installed
@@ -91,69 +91,31 @@ do
   fi
 done
 
-for command in libtool14 libtool15 libtool glibtool 
-do
-  URL=$gnu/$pkg/
-  if
-    testProgram $command
-  then
-    libtool=$command
-    libtoolize=`echo  "$libtool" | sed -e 's/libtool/libtoolize/'`
-  fi
-done
-
-if [ -z $autoconf ]; then 
-    echo You must have autoconf installed to compile the cluster-glue package.
+if [ -z $autoconf ]; then
+    echo You must have autoconf installed to compile the crmsh package.
     echo Download the appropriate package for your system,
     echo or get the source tarball at: $gnu/autoconf/
     exit 1
 
-elif [ -z $automake ]; then 
-    echo You must have automake installed to compile the cluster-glue package.
+elif [ -z $automake ]; then
+    echo You must have automake installed to compile the crmsh package.
     echo Download the appropriate package for your system,
     echo or get the source tarball at: $gnu/automake/
     exit 1
 
-elif [ -z $libtool ]; then 
-    echo You must have libtool installed to compile the cluster-glue package.
-    echo Download the appropriate package for your system,
-    echo or get the source tarball at: $gnu/libtool/
-    exit 1
-fi
-
-oneline() {
-  read x; echo "$x"
-}
-
-LT_version=`$libtool --version | oneline | sed -e 's%^[^0-9]*%%' -e s'% .*%%'`
-LT_majvers=`echo "$LT_version" | sed -e 's%\..*%%'`
-LT_minvers=`echo "$LT_version" | sed -e 's%^[^.]*\.%%' `
-LT_minnum=`echo  "$LT_minvers" | sed -e 's%[^0-9].*%%'`
-
-if
-  [ $LT_majvers -lt 1 ] || [ $LT_majvers = 1 -a $LT_minnum -lt 4 ]
-then
-  echo "Minimum version of libtool is 1.4.  You have $LT_version installed."
-  exit 1
 fi
 
 # Create local copies so that the incremental updates will work.
-rm -f ./autoconf ./automake ./autoheader ./libtool
-ln -s `which $libtool` ./libtool
+rm -f ./autoconf ./automake ./autoheader
 ln -s `which $autoconf` ./autoconf
 ln -s `which $automake` ./automake
 ln -s `which $autoheader` ./autoheader
 
 printf "$autoconf:\t"
-$autoconf --version | head -n 1 
+$autoconf --version | head -n 1
 
 printf "$automake:\t"
 $automake --version | head -n 1
-
-#rm -rf libltdl libltdl.tar
-#echo $libtoolize --ltdl --force --copy
-# Unset GREP_OPTIONS as any coloring can mess up the AC_CONFIG_AUX_DIR matching patterns
-#GREP_OPTIONS= $libtoolize --ltdl --force --copy
 
 arch=`uname -s`
 # Disable the errors on FreeBSD until a fix can be found.
@@ -169,11 +131,6 @@ set -e
 trap 'echo ""; echo "$0 exiting due to error (sorry!)." >&2' 0
 fi
 
-# Emulate the old --ltdl-tar option...
-#  If the libltdl directory is required we will unpack it later
-#tar -cf libltdl.tar libltdl
-#rm -rf libltdl
-
 echo $aclocal $ACLOCAL_FLAGS
 $aclocal $ACLOCAL_FLAGS
 
@@ -182,9 +139,6 @@ $automake --add-missing --include-deps --copy
 
 echo $autoconf
 $autoconf
-
-#test -f libtool.m4 || touch libtool.m4 
-#test -f ltdl.m4 || touch ltdl.m4
 
 echo Now run ./configure
 trap '' 0
