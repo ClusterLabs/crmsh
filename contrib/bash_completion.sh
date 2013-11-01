082415fc3a2602f1ccb01380353d95927481f9b2
Kristoffer Grönlund <kgronlund@suse.com>
2013-11-01 09:56:03 +0100

    Low: bash_completion.sh: Silence errors

--- contrib/bash_completion.sh
+++ contrib/bash_completion.sh
@@ -6,7 +6,6 @@ _crm() {
     cur="$2"
     prev="$3"
 
-    
     for ((i=1; $i<=$COMP_CWORD; i++)); do
         if [[ ${COMP_WORDS[i]} != -* ]]; then
             if [[ ${COMP_WORDS[i-1]} != @(-f|--file|-H|--history|-D|--display|-X|-c|--cib) ]]; then
@@ -16,19 +15,19 @@ _crm() {
             fi
         fi
     done
-    
+
     case $prev in
         -f|--file|-H|--history|-D|--display|-X|-c|--cib)
             # use default completion
             return
             ;;
     esac
-    
+
     if [[ "$cur" == -* ]]; then
         COMPREPLY=(${COMPREPLY[@]:-} $(compgen -W '-w -h -d -F -R -f --file -H --history -D --display -X -c --cib' -- "$cur"))
         return 0
     fi
-    
-    COMPREPLY=(${COMPREPLY[@]:-} $(compgen -W '$(./crm --compgen "${COMP_POINT}" "${COMP_LINE}")' -- "$cur"))
+
+    COMPREPLY=(${COMPREPLY[@]:-} $(compgen -W '$(2>/dev/null ./crm --compgen "${COMP_POINT}" "${COMP_LINE}")' -- "$cur"))
 } &&
 complete -o bashdefault -o default -F _crm crm     || complete -o default -F _crm crm
