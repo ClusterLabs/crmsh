1270ae932f2858fd5c6c0a4b45a69ccc9eab6567
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-14 01:04:15 +0100

    Low: scripts: Revised health command

--- scripts/health/collect.py
+++ scripts/health/collect.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import crm_script
 data = crm_script.get_input()
 
@@ -26,14 +27,62 @@ PACKAGES = ['booth',
             'rubygem-sinatra',
             'sbd']
 
-try:
-    rpm_states = crm_script.rpmcheck(PACKAGES)
+def rpm_info():
+    return crm_script.rpmcheck(PACKAGES)
+
+def package_info():
     package_states = []
     for pkg in PACKAGES:
         package_states.append(crm_script.check_package(pkg, state='present'))
+    return package_states
 
-    data = {'rpm': rpm_states,
-            'packages': package_states}
+def logrotate_info():
+    rc, _, _ = crm_script.call(
+        'grep -r corosync.conf /etc/logrotate.d',
+        shell=True)
+    return {'corosync.conf': rc == 0}
+
+def sys_info():
+    sysname, nodename, release, version, machine = os.uname()
+    return {'sysname': sysname,
+            'nodename': nodename,
+            'release': release,
+            'version': version,
+            'machine': machine}
+
+def disk_info():
+    rc, out, err = crm_script.call(['df'], shell=False)
+    if rc == 0:
+        disk_use = []
+        for line in out.split('\n')[1:]:
+            line = line.strip()
+            if line:
+                data = line.split()
+                if len(data) >= 6:
+                    disk_use.append((data[5], data[4]))
+        return disk_use
+    return []
+"""
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sdb3       197G  100G   88G  54% /
+devtmpfs        7.9G   56K  7.9G   1% /dev
+tmpfs           7.9G  1.1M  7.9G   1% /dev/shm
+tmpfs           7.9G   11M  7.9G   1% /run
+tmpfs           7.9G     0  7.9G   0% /sys/fs/cgroup
+tmpfs           7.9G   11M  7.9G   1% /var/run
+tmpfs           7.9G   11M  7.9G   1% /var/lock
+/dev/sdb1       195M  128K  195M   1% /boot/efi
+/dev/sdb4       697G  310G  352G  47% /home
+"""
+
+try:
+    data = {
+        'rpm': rpm_info(),
+        'packages': package_info(),
+        'logrotate': logrotate_info(),
+        'system': sys_info(),
+        'disk': disk_info()
+    }
     crm_script.exit_ok(data)
 except Exception, e:
     crm_script.exit_fail(str(e))
--- scripts/health/report.py
+++ scripts/health/report.py
@@ -3,24 +3,66 @@ import crm_script
 data = crm_script.get_input()
 health_report = data[1]
 
+print "Processing collected information..."
+
+CORE_PACKAGES = ['corosync', 'pacemaker', 'resource-agents']
+
+warnings = []
+errors = []
+
+def warn(fmt, *args):
+    warnings.append(fmt % args)
+
+def error(fmt, *args):
+    errors.append(fmt % args)
+
+# sort {package: {version: [host]}}
 rpm_versions = {}
 
 for node, info in health_report.iteritems():
     for rpm in info['rpm']:
-        if rpm['name'] not in rpm_versions:
-            rpm_versions[rpm['name']] = {}
         if 'error' in rpm:
-            rpm_versions[rpm['name']][node] = None
+            if rpm['name'] not in rpm_versions:
+                rpm_versions[rpm['name']] = {rpm['error']: [node]}
+            else:
+                versions = rpm_versions[rpm['name']]
+                if rpm['error'] in versions:
+                    versions[rpm['error']].append(node)
+                else:
+                    versions[rpm['error']] = [node]
         else:
-            rpm_versions[rpm['name']][node] = rpm['version']
+            if rpm['name'] not in rpm_versions:
+                rpm_versions[rpm['name']] = {rpm['version']: [node]}
+            else:
+                versions = rpm_versions[rpm['name']]
+                if rpm['version'] in versions:
+                    versions[rpm['version']].append(node)
+                else:
+                    versions[rpm['version']] = [node]
+    for disk, use in info['disk']:
+        use = int(use[:-1])
+        if use > 90:
+            warn("On %s, disk %s usage is %s%%", node, disk, use)
 
-print "RPM versions:"
-for name, rpm in rpm_versions.iteritems():
-    for k, v in rpm.iteritems():
-        if v is not None:
-            break
-    else:
-        print "%s: Not installed" % (name)
-        continue
-    print "%s: %s" % (name, ', '.join('%s:%s' % (k, v) for k, v in rpm.iteritems()))
-print "Health check complete."
+for cp in CORE_PACKAGES:
+    if cp not in rpm_versions:
+        error("Core package '%s' not installed on any node", cp)
+
+for name, versions in rpm_versions.iteritems():
+    if len(versions) > 1:
+        desc = ', '.join('%s (%s)' % (v, ', '.join(nodes)) for v, nodes in versions.items())
+        warn("Package %s: Versions differ! %s", name, desc)
+
+    all_hosts = set(sum([hosts for hosts in versions.values()], []))
+    for node in health_report.keys():
+        if len(all_hosts) > 0 and node not in all_hosts:
+            warn("Package '%s' not installed on host '%s'" % (name, node))
+
+if errors:
+    for e in errors:
+        print "ERROR:", e
+if warnings:
+    for w in warnings:
+        print "WARNING:", w
+if not errors and not warnings:
+    print "No issues found."
