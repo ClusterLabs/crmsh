fae70fd532bfc3322e5069d02d66ef0cce1a3173
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-16 16:35:08 +0100

    Doc: Update cluster script example and documentation

--- scripts/check-uptime/fetch.py
+++ scripts/check-uptime/fetch.py
@@ -2,6 +2,6 @@
 import crm_script
 try:
     uptime = open('/proc/uptime').read().split()[0]
+    crm_script.exit_ok(uptime)
 except:
     crm_script.exit_fail("Couldn't open /proc/uptime")
-crm_script.exit_ok({'uptime': uptime})
--- scripts/check-uptime/report.py
+++ scripts/check-uptime/report.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 import crm_script
-data = crm_script.get_input()
-show_all = crm_script.is_true(data[0]['show_all'])
-uptimes = [(h, v['uptime']) for h, v in data[1].iteritems()]
+show_all = crm_script.is_true(crm_script.param('show_all'))
+uptimes = crm_script.output(1).items()
 max_uptime = '', 0
-for h, t in uptimes:
-    if t > max_uptime[1]:
-        max_uptime = h, t
+for host, uptime in uptimes:
+    if uptime > max_uptime[1]:
+        max_uptime = host, uptime
 if show_all:
     print "Uptimes: %s" % (', '.join("%s: %s" % v for v in uptimes))
 print "Longest uptime is %s seconds on host %s" % (max_uptime[1], max_uptime[0])
