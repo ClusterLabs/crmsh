72aa2df99da442955b5d65fafef5b9befa74c9f1
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-16 16:35:08 +0100

    Medium: scripts: New node remove script

---
+++ scripts/remove/remove.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import crm_script
+
+host = crm_script.host()
+node = crm_script.param('node')
+
+
+def run_collect():
+    crm_script.exit_ok(host)
+
+
+def run_validate():
+    data = crm_script.output(1)
+    if data.get(node) != node:
+        crm_script.exit_fail("%s not found or not responding: %s" % (node, data.get(node)))
+    if host == node:
+        crm_script.exit_fail("Call from another node: %s = %s" % (node, host))
+    crm_script.exit_ok(node)
+
+
+def run_apply():
+    rc, out, err = crm_script.call(['ssh',
+                                    'root@%s' % (node),
+                                    'systemctl stop corosync.service'])
+    if rc != 0:
+        crm_script.exit_fail("Failed to stop corosync on %s: %s" % (node, err))
+
+    rc, out, err = crm_script.call(['crm', 'node', 'delete', node])
+    if rc != 0:
+        crm_script.exit_fail("Failed to remove %s from CIB: %s" % (node, err))
+
+    crm_script.exit_ok({"removed": node})
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or sys.argv[1] == 'collect':
+        run_collect()
+    elif sys.argv[1] == 'validate':
+        run_validate()
+    elif sys.argv[1] == 'apply':
+        run_apply()
+    else:
+        crm_script.exit_fail("Unknown argument: %s" % sys.argv[1])
