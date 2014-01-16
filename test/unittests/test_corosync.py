e1bbb9aca1e8b0c2a5f03c1fa6680adfe85a0f61
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-16 16:35:08 +0100

    Dev: tests: Unit tests for corosync parser

---
+++ test/unittests/test_corosync.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2013 Kristoffer Gronlund <kgronlund@suse.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# unit tests for parse.py
+
+import unittest
+import corosync
+from corosync import Parser, make_section, make_value
+
+
+F1 = open('corosync.conf.1').read()
+F2 = open('corosync.conf.2').read()
+
+
+def _valid(parser):
+    depth = 0
+    for t in parser._tokens:
+        if t.token not in (corosync._tCOMMENT,
+                           corosync._tBEGIN,
+                           corosync._tEND,
+                           corosync._tVALUE):
+            raise AssertionError("illegal token " + str(t))
+        if t.token == corosync._tBEGIN:
+            depth += 1
+        if t.token == corosync._tEND:
+            depth -= 1
+    if depth != 0:
+        raise AssertionError("Unbalanced sections")
+
+
+def _print(parser):
+    print parser.to_string()
+
+
+class TestCorosyncParser(unittest.TestCase):
+    def test_parse(self):
+        p = Parser(F1)
+        _valid(p)
+        self.assertEqual(p.get('logging.logfile'), '/var/log/cluster/corosync.log')
+        self.assertEqual(p.get('totem.interface.ttl'), '1')
+        p.set('totem.interface.ttl', '2')
+        _valid(p)
+        self.assertEqual(p.get('totem.interface.ttl'), '2')
+        p.remove('quorum')
+        _valid(p)
+        self.assertEqual(p.count('quorum'), 0)
+        p.add('', make_section('quorum', []))
+        _valid(p)
+        self.assertEqual(p.count('quorum'), 1)
+        p.set('quorum.votequorum', '2')
+        _valid(p)
+        self.assertEqual(p.get('quorum.votequorum'), '2')
+        p.set('bananas', '5')
+        _valid(p)
+        self.assertEqual(p.get('bananas'), '5')
+
+    def test_logfile(self):
+        self.assertEqual(corosync.logfile(F1), '/var/log/cluster/corosync.log')
+        self.assertEqual(corosync.logfile('# nothing\n'), None)
+
+    def test_udpu(self):
+        p = Parser(F2)
+        _valid(p)
+        self.assertEqual(p.count('nodelist.node'), 5)
+        p.add('nodelist',
+              make_section('nodelist.node',
+                           make_value('nodelist.node.ring0_addr', '10.10.10.10') +
+                           make_value('nodelist.node.nodeid', str(corosync.next_nodeid(p)))))
+        _valid(p)
+        self.assertEqual(p.count('nodelist.node'), 6)
+        self.assertEqual(p.get_all('nodelist.node.nodeid'),
+                         ['1', '2', '3'])
+
+    def test_remove_node(self):
+        p = Parser(F2)
+        _valid(p)
+        self.assertEqual(p.count('nodelist.node'), 5)
+        p.remove_section_where('nodelist.node', 'nodeid', '2')
+        _valid(p)
+        self.assertEqual(p.count('nodelist.node'), 4)
+        self.assertEqual(p.get_all('nodelist.node.nodeid'),
+                         ['1'])
+
+if __name__ == '__main__':
+    unittest.main()
