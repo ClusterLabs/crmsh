d9ab07e06f33ff265f609714ed0dd2c6282a3c2e
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-16 16:35:08 +0100

    dev: utils: clean up crm_script.py

--- utils/crm_script.py
+++ utils/crm_script.py
@@ -5,48 +5,75 @@ try:
     import json
 except ImportError:
     import simplejson as json
+
+_input = None
+
+
 def call(cmd, shell=False):
     p = proc.Popen(cmd, shell=shell, stdin=None, stdout=proc.PIPE, stderr=proc.PIPE)
     out, err = p.communicate()
     return p.returncode, out.strip(), err.strip()
+
+
 def host():
     return os.uname()[1]
-_input = None
+
+
 def get_input():
     global _input
     if _input is None:
-        _input =  json.load(open('./script.input'))
+        _input = json.load(open('./script.input'))
     return _input
+
+
 def parameters():
     return get_input()[0]
+
+
 def param(name):
     return parameters().get(name)
+
+
 def output(step_idx):
     if step_idx < len(get_input()):
         return get_input()[step_idx]
     return {}
+
+
 def exit_fail(msg):
     print >>sys.stderr, msg
     sys.exit(1)
+
+
 def exit_ok(data):
     print json.dumps(data)
     sys.exit(0)
+
+
 def is_true(s):
     if s in (True, False):
         return s
     return s.lower() in ('yes', 'true', '1', 'on')
+
+
 def service(name, action):
     return call(['systemctl', action, name + '.service'])
+
+
 def package(name, state):
     rc, out, err = call(['./crm_pkg.py', '-n', name, '-s', state])
     if rc != 0:
         raise IOError(err)
     return json.loads(out)
+
+
 def check_package(name, state):
     rc, out, err = call(['./crm_pkg.py', '--dry-run', '-n', name, '-s', state])
     if rc != 0:
         raise IOError(err)
     return json.loads(out)
+
+
 def rpmcheck(names):
     rc, out, err = call(['./crm_rpmcheck.py'] + names)
     if rc != 0:
