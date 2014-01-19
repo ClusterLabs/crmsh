637006b6f7305e1c96891ad352f4846a735a6e6a
Kristoffer Grönlund <kgronlund@suse.com>
2014-01-19 17:42:48 +0100

    low: scripts: Print error details on uptime script

--- scripts/check-uptime/fetch.py
+++ scripts/check-uptime/fetch.py
@@ -3,5 +3,5 @@ import crm_script
 try:
     uptime = open('/proc/uptime').read().split()[0]
     crm_script.exit_ok(uptime)
-except:
-    crm_script.exit_fail("Couldn't open /proc/uptime")
+except Exception, e:
+    crm_script.exit_fail("Couldn't open /proc/uptime: %s" % (e))
