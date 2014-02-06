40c95b178c8b831f0368db900d62262767660f17
Kristoffer Grönlund <kgronlund@suse.com>
2014-02-06 15:15:07 +0100

    test: utils: Some unit test coverage for utils.py

---
+++ test/unittests/test_utils.py
@@ -0,0 +1,137 @@
+# Copyright (C) 2014 Kristoffer Gronlund <kgronlund@suse.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# unit tests for utils.py
+
+import os
+from itertools import chain
+import utils
+import config
+
+
+def test_systeminfo():
+    assert utils.getuser() is not None
+    assert utils.gethomedir() is not None
+    assert utils.get_tempdir() is not None
+
+
+def test_shadowcib():
+    assert utils.get_cib_in_use() == ""
+    utils.set_cib_in_use("foo")
+    assert utils.get_cib_in_use() == "foo"
+    utils.clear_cib_in_use()
+    assert utils.get_cib_in_use() == ""
+
+
+def test_booleans():
+    truthy = ['yes', 'Yes', 'True', 'true', 'TRUE',
+              'YES', 'on', 'On', 'ON']
+    falsy = ['no', 'false', 'off', 'OFF', 'FALSE', 'nO']
+    not_truthy = ['', 'not', 'ONN', 'TRUETH', 'yess']
+    for case in chain(truthy, falsy):
+        assert utils.verify_boolean(case) is True
+    for case in truthy:
+        assert utils.is_boolean_true(case) is True
+        assert utils.is_boolean_false(case) is False
+        assert utils.get_boolean(case) is True
+    for case in falsy:
+        assert utils.is_boolean_true(case) is False
+        assert utils.is_boolean_false(case) is True
+        assert utils.get_boolean(case, dflt=True) is False
+    for case in not_truthy:
+        assert utils.verify_boolean(case) is False
+        assert utils.is_boolean_true(case) is False
+        assert utils.is_boolean_false(case) is False
+        assert utils.get_boolean(case) is False
+
+
+def test_olist():
+    lst = utils.olist(['B', 'C', 'A'])
+    lst.append('f')
+    lst.append('aA')
+    lst.append('_')
+    assert 'aa' in lst
+    assert 'a' in lst
+    assert list(lst) == ['b', 'c', 'a', 'f', 'aa', '_']
+
+
+def test_add_sudo():
+    tmpuser = config.core.user
+    try:
+        config.core.user = 'root'
+        assert utils.add_sudo('ls').startswith('sudo')
+        config.core.user = ''
+        assert utils.add_sudo('ls') == 'ls'
+    finally:
+        config.core.user = tmpuser
+
+
+def test_str2tmp():
+    txt = "This is a test string"
+    filename = utils.str2tmp(txt)
+    assert os.path.isfile(filename)
+    assert open(filename).read() == txt + "\n"
+    assert utils.file2str(filename) == txt
+    # TODO: should this really return
+    # an empty line at the end?
+    assert utils.file2list(filename) == [txt, '']
+    os.unlink(filename)
+
+
+def test_sanity():
+    sane_paths = ['foo/bar', 'foo', '/foo/bar', 'foo0',
+                  'foo_bar', 'foo-bar', '0foo', '.foo',
+                  'foo.bar']
+    insane_paths = ['#foo', 'foo?', 'foo*', 'foo$', 'foo[bar]',
+                    'foo`', "foo'", 'foo/*']
+    for p in sane_paths:
+        assert utils.is_path_sane(p)
+    for p in insane_paths:
+        assert not utils.is_path_sane(p)
+    sane_filenames = ['foo', '0foo', '0', '.foo']
+    insane_filenames = ['foo/bar']
+    for p in sane_filenames:
+        assert utils.is_filename_sane(p)
+    for p in insane_filenames:
+        assert not utils.is_filename_sane(p)
+    sane_names = ['foo']
+    insane_names = ["f'o"]
+    for n in sane_names:
+        assert utils.is_name_sane(n)
+        assert utils.is_value_sane(n)
+    for n in insane_names:
+        assert not utils.is_name_sane(n)
+        assert not utils.is_value_sane(n)
+
+
+def test_nvpairs2dict():
+    assert utils.nvpairs2dict(['a=b', 'c=d']) == {'a': 'b', 'c': 'd'}
+    assert utils.nvpairs2dict(['a=b=c', 'c=d']) == {'a': 'b=c', 'c': 'd'}
+    assert utils.nvpairs2dict(['a']) == {'a': None}
+
+
+def test_validity():
+    assert utils.is_id_valid('foo0')
+    assert not utils.is_id_valid('0foo')
+
+
+def test_msec():
+    assert utils.crm_msec('1ms') == 1
+    assert utils.crm_msec('1s') == 1000
+    assert utils.crm_msec('1us') == 0
+    assert utils.crm_msec('1') == 1000
+    assert utils.crm_msec('1m') == 60*1000
+    assert utils.crm_msec('1h') == 60*60*1000
