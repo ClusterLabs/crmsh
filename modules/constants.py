240e14a1b0336df2e0d042017861b7cc4e345727
Kristoffer Grönlund <krig@koru.se>
2014-08-09 09:06:29 +0200

    low: constants: Add requires to list of meta attributes

--- modules/constants.py
+++ modules/constants.py
@@ -145,7 +145,7 @@ rsc_meta_attributes = (
     "allow-migrate", "is-managed", "interval-origin",
     "migration-threshold", "priority", "multiple-active",
     "failure-timeout", "resource-stickiness", "target-role",
-    "restart-type", "description", "remote-node",
+    "restart-type", "description", "remote-node", "requires",
 )
 group_meta_attributes = ("container", )
 clone_meta_attributes = (
