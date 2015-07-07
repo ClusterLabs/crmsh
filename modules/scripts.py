0ed3e173918c801f06159570045fefc90dce9193
Kristoffer Grönlund <krig@koru.se>
2015-07-07 18:08:45 +0200

    dev: Reference COPYING in license headers [2]

--- modules/scripts.py
+++ modules/scripts.py
@@ -1,23 +1,5 @@
 # Copyright (C) 2015 Kristoffer Gronlund <kgronlund@suse.com>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-#
-# A rewrite of cluster scripts with more functionality built in
-# (including a mustashe-like templating language), and a JSON API
-# for Hawk to use.
+# See COPYING for license information.
 
 import os
 import re
