686e3ec625e97355d765ebfc082694fed4deb005
Kristoffer Grönlund <krig@koru.se>
2015-10-26 13:39:47 +0100

    test: Time test case to try to remember convoluted logic used

---
+++ test/unittests/test_time.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2014 Kristoffer Gronlund <kgronlund@suse.com>
+# See COPYING for license information.
+
+
+from crmsh import utils
+from crmsh import report
+from nose.tools import eq_
+import time
+import datetime
+import dateutil.tz
+
+
+def test_time_convert1():
+    loctz = dateutil.tz.tzlocal()
+    tm = time.localtime(utils.datetime_to_timestamp(utils.make_datetime_naive(datetime.datetime(2015, 6, 1, 10, 0, 0).replace(tzinfo=loctz))))
+    dt = utils.parse_time('Jun 01, 2015 10:00:00')
+    eq_(report.human_date(dt), time.strftime('%Y-%m-%d %H:%M:%S', tm))
