0988ac4d1d2b42a705640bd946f3eb9420258d65
Kristoffer Grönlund <krig@koru.se>
2016-01-15 17:12:40 +0100

    dev: Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ import os
 SRC_PATH = os.path.relpath(os.path.join(os.path.dirname(__file__), "modules"))
 
 setup(name='crmsh',
-      version='2.2.0-rc3',
+      version='2.2.0',
       description='Command-line interface for High-Availability cluster management',
-      author='Dejan Muhamedagic',
-      author_email='dejan@suse.de',
+      author='Kristoffer Gronlund',
+      author_email='kgronlund@suse.com',
       url='http://crmsh.github.io/',
       packages=['crmsh'],
       package_dir={'crmsh': SRC_PATH})
