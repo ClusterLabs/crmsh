d333db8819c293719563f728ed1ccfa60cdf74a2
Dejan Muhamedagic <dejan@suse.de>
2016-01-21 14:39:28 +0100

    Medium: history: update patterns for resource (new lrmd)

--- modules/log_patterns_118.py
+++ modules/log_patterns_118.py
@@ -25,12 +25,17 @@ log_patterns = {
         (  # detail 0
             "crmd.*Initiating.*%%_(?:start|stop|promote|demote|migrate)_",
             "lrmd.*operation_finished: %%_",
+            "lrmd.*executing - rsc:%% action:(?:start|stop|promote|demote|migrate)",
+            "lrmd.*finished - rsc:%% action:(?:start|stop|promote|demote|migrate)",
+
             "crmd.*LRM operation %%_(?:start|stop|promote|demote|migrate)_.*confirmed=true",
             "crmd.*LRM operation %%_.*Timed Out",
             "[(]%%[)][[]",
         ),
         (  # detail 1
-            "crmd.*Initiating%%_(?:monitor_0|notify)",
+            "crmd.*Initiating.*%%_(?:monitor_0|notify)",
+            "lrmd.*executing - rsc:%% action:(?:monitor_0|notify)",
+            "lrmd.*finished - rsc:%% action:(?:monitor_0|notify)",
         ),
     ),
     "node": (
@@ -74,3 +79,5 @@ log_patterns = {
         ),
     ),
 }
+
+# vim:ts=4:sw=4:et:
