ClusterLabs/crmsh

commit 2a9a68466c5416de3e0ff67a295396a3ffd40eaf

dev: ui_cluster: More reasonable list/tuple handling

crmsh/ui_cluster.py
@@ -287,10 +287,10 @@ If stage is not specified, each stage will be invoked in sequence.
         parser.add_option("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")
         parser.add_option("-c", "--cluster-node", dest="cluster_node", help="IP address or hostname of cluster node which will be deleted", metavar="HOST")
 
-        args = list(args)
+        args = args
         options, args = parser.parse_args(args)
         if options.cluster_node is not None and options.cluster_node not in args:
-            args.append(options.cluster_node)
+            args = list(args) + [options.cluster_node]
         if len(args) == 0:
             bootstrap.bootstrap_remove(
                 cluster_node=None,

commit 165026a45e906f393a15a60e680025c8c2d25c2b

medium: ui_cluster: Fix broken cluster remove command
Commit 2a9a68466c5416de3e0ff67a295396a3ffd40eaf broke
the remove command by passing a tuple to parse_args().

crmsh/ui_cluster.py
@@ -288,8 +288,7 @@ If stage is not specified, each stage will be invoked in sequence.
         parser.add_option("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")
         parser.add_option("-c", "--cluster-node", dest="cluster_node", help="IP address or hostname of cluster node which will be deleted", metavar="HOST")
 
-        args = args
-        options, args = parser.parse_args(args)
+        options, args = parser.parse_args(list(args))
         if options.cluster_node is not None and options.cluster_node not in args:
             args = list(args) + [options.cluster_node]
         if len(args) == 0: