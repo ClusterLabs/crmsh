72e6d2027945e7b0f295124b331139a39a332d9d
liangxin1300 <XLiang@suse.com>
2017-06-15 11:49:27 +0800

    add prompt_toolkit in travis-tests.sh

--- test/travis-tests.sh
+++ test/travis-tests.sh
@@ -5,7 +5,7 @@ set -ev
 sudo make install
 sudo cp -f /usr/bin/crm /usr/sbin/crm
 if [  "$TRAVIS_PYTHON_VERSION" = "2.7_with_system_site_packages" ]; then
-	pip -v --isolated install parallax
+	pip -v --isolated install parallax prompt_toolkit
 	sudo /usr/share/crmsh/tests/regression.sh
 	cd /usr/share/crmsh/tests
 	sudo ./cib-tests.sh
