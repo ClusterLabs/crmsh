3f7dc3f4d351db61d849474f3f252e7f854fc563
Kristoffer Grönlund <krig@koru.se>
2017-11-01 08:57:20 +0100

    dev: Preserve UID/GID when configuring/making in container

--- test/run-in-container.sh
+++ test/run-in-container.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+oname=$1
+ouid=$2
+ogid=$3
+
+cat /etc/group | awk '{ FS = ":" } { print $3 }' | grep -q $ogid || groupadd -g $ogid
+id -u $oname >/dev/null 2>&1 || useradd -u $ouid -g $ogid $oname
+
 echo "** Unit tests"
-./test/run
+su $oname -c "./test/run"
 echo "** Autogen / Configure"
-./autogen.sh
-./configure --prefix /usr
+su $oname -c "./autogen.sh"
+su $oname -c "./configure --prefix /usr"
 echo "** Make / Install"
 make install
 echo "** Regression tests"
