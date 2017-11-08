41671c61c87eae1cf775475702ec2a10fb986a84
liangxin1300 <XLiang@suse.com>
2017-11-08 17:15:40 +0800

    low: ui_configure: Improve do_group completer

--- crmsh/utils.py
+++ crmsh/utils.py
@@ -42,13 +42,11 @@ def to_ascii(s):
         return s
 
 
-def filter_exist_args(args, params_can_be_remove):
-    for item in args:
-        for start in params_can_be_remove:
-            if item.startswith(start):
-                params_can_be_remove.remove(start)
-                continue
-    return params_can_be_remove
+def not_completed(item, args, use='='):
+    for arg in args:
+        if arg.startswith(item+use):
+            return False
+    return True
 
 
 def memoize(function):
