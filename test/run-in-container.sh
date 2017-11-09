afe8d070354c2c13c6e2410c760a8fb46d4a2304
Kristoffer Grönlund <krig@koru.se>
2017-11-09 18:52:10 +0100

    test: structure the container test script

--- test/run-in-container.sh
+++ test/run-in-container.sh
@@ -7,12 +7,28 @@ ogid=$3
 cat /etc/group | awk '{ FS = ":" } { print $3 }' | grep -q $ogid || groupadd -g $ogid
 id -u $oname >/dev/null 2>&1 || useradd -u $ouid -g $ogid $oname
 
-echo "** Unit tests"
-su $oname -c "./test/run"
-echo "** Autogen / Configure"
-su $oname -c "./autogen.sh"
-su $oname -c "./configure --prefix /usr"
-echo "** Make / Install"
-make install
-echo "** Regression tests"
-sh /usr/share/crmsh/tests/regression.sh
+unit_tests() {
+	echo "** Unit tests"
+	su $oname -c "./test/run"
+}
+
+configure() {
+	echo "** Autogen / Configure"
+	su $oname -c "./autogen.sh"
+	su $oname -c "./configure --prefix /usr"
+}
+
+make_install() {
+	echo "** Make / Install"
+	make install
+}
+
+regression_tests() {
+	echo "** Regression tests"
+	sh /usr/share/crmsh/tests/regression.sh
+}
+
+unit_tests
+configure
+make_install
+regression_tests
