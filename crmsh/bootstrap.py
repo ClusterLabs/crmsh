3e8a4e72c46481fd26654367fb2f603fd8498310
Kristoffer Grönlund <krig@koru.se>
2017-12-04 10:05:10 +0100

    Fix is_program(dmidecode) error (bsc#1070344)

--- crmsh/bootstrap.py
+++ crmsh/bootstrap.py
@@ -1040,7 +1040,7 @@ def init_corosync():
     """
     @utils.memoize
     def check_amazon():
-        if not is_program("dmidecode"):
+        if not utils.is_program("dmidecode"):
             return False
         _rc, outp = utils.get_stdout("dmidecode -s system-version")
         return re.search(r"\<.*\.amazon\>", outp) is not None
