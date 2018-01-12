9561209c80e799683eda21e4fc93c9740db8e42e
Kristoffer Grönlund <krig@koru.se>
2018-01-12 15:29:03 +0100

    test: Clean up output script

--- test/update-expected-output.sh
+++ test/update-expected-output.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 crmtestout="$1"
 
+[ -d "$crmtestout" ] || { echo "usage: $0 <test-output-dir>"; exit 1; }
+
 for f in $crmtestout/*.diff; do
 	fil=$(grep -- --- $f | awk '{print $2}' | sed 's/\/usr\/share\/crmsh\/tests/\/test/g')
-
-	cat $f | awk "NR==1{\$2=\"a$fil\"}1" | awk "NR==2{\$2=\"b$fil\"}1"
-
-
+	awk "NR==1{\$2=\"a$fil\"}1" < "$f" | awk "NR==2{\$2=\"b$fil\"}1"
 done
