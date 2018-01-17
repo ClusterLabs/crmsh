c1bd8100fb8ad5f5d1f55929cb2dc84125606699
Kristoffer Grönlund <krig@koru.se>
2018-01-17 10:39:19 +0100

    test: Update Travis configuration

--- test/run-in-container.sh
+++ test/run-in-container.sh
@@ -4,7 +4,7 @@ oname=$1
 ouid=$2
 ogid=$3
 
-cat /etc/group | awk '{ FS = ":" } { print $3 }' | grep -q $ogid || groupadd -g $ogid
+cat /etc/group | awk '{ FS = ":" } { print $3 }' | grep -q "$ogid" || groupadd -g "$ogid"
 id -u $oname >/dev/null 2>&1 || useradd -u $ouid -g $ogid $oname
 
 unit_tests() {
