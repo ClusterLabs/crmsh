24b2374d910ce80b8197eff0db54a962d93af3da
Valentin Vidic <Valentin.Vidic@CARNet.hr>
2019-01-13 19:56:16 +0100

    low: utils: add support for dpkg
    For checking package status on Debian and Ubuntu.

--- utils/crm_rpmcheck.py
+++ utils/crm_rpmcheck.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2013 Kristoffer Gronlund <kgronlund@suse.com>
 # See COPYING for license information.
 
+import os
 import sys
 import json
 import subprocess
@@ -22,6 +23,19 @@ def package_data(pkg):
     """
     Gathers version and release information about a package.
     """
+    if os.path.isfile('/bin/rpm'):
+        return rpm_package_data(pkg)
+
+    if os.path.isfile('/usr/bin/dpkg'):
+        return dpkg_package_data(pkg)
+
+    return {'name': pkg, 'error': "unknown package manager"}
+
+
+def rpm_package_data(pkg):
+    """
+    Gathers version and release information about an RPM package.
+    """
     _qfmt = 'version: %{VERSION}\nrelease: %{RELEASE}\n'
     rc, out, err = run(['/bin/rpm', '-q', '--queryformat=' + _qfmt, pkg])
     if rc == 0:
@@ -30,6 +44,22 @@ def package_data(pkg):
             info = line.split(':', 1)
             if len(info) == 2:
                 data[info[0].strip()] = info[1].strip()
+        return data
+    else:
+        return {'name': pkg, 'error': "package not installed"}
+
+
+def dpkg_package_data(pkg):
+    """
+    Gathers version and release information about a DPKG package.
+    """
+    rc, out, err = run(['/usr/bin/dpkg', '--status', pkg])
+    if rc == 0:
+        data = {'name': pkg}
+        for line in out.split('\n'):
+            info = line.split(':', 1)
+            if len(info) == 2:
+                data[info[0].strip().lower()] = info[1].strip()
         return data
     else:
         return {'name': pkg, 'error': "package not installed"}
