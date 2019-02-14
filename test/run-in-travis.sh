3fe3787e03766d4f0dafcbd3ec8b7247d7254b15
Kristoffer Grönlund <krig@koru.se>
2019-02-14 09:26:18 +0100

    test: Use containerized test setup in Travis

---
+++ test/run-in-travis.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+unit_tests() {
+	echo "** Unit tests"
+	./test/run
+}
+
+configure() {
+	echo "** Autogen / Configure"
+	./autogen.sh
+	./configure --prefix /usr
+}
+
+make_install() {
+	echo "** Make / Install"
+	make install
+}
+
+regression_tests() {
+	echo "** Regression tests"
+	sh /usr/share/crmsh/tests/regression.sh
+}
+
+unit_tests
+rc_unittest=$?
+configure
+make_install
+regression_tests && exit $rc_unittest
