652f710ebac365e56c9fc2cf7ae50d1c0a90a357
liangxin1300 <XLiang@suse.com>
2020-10-22 10:41:30 +0800

    Dev: unittest: unit test for function do_start/do_stop

---
+++ test/unittests/test_ui_cluster.py
@@ -0,0 +1,79 @@
+import unittest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from crmsh import ui_cluster
+
+
+class TestCluster(unittest.TestCase):
+    """
+    Unitary tests for class utils.IP
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Global setUp.
+        """
+
+    def setUp(self):
+        """
+        Test setUp.
+        """
+        self.ui_cluster_inst = ui_cluster.Cluster()
+
+    def tearDown(self):
+        """
+        Test tearDown.
+        """
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Global tearDown.
+        """
+
+    @mock.patch('crmsh.ui_cluster.err_buf.info')
+    @mock.patch('crmsh.utils.service_is_active')
+    def test_do_start_already_started(self, mock_active, mock_info):
+        context_inst = mock.Mock()
+        mock_active.return_value = True
+        self.ui_cluster_inst.do_start(context_inst)
+        mock_active.assert_called_once_with("pacemaker.service")
+        mock_info.assert_called_once_with("Cluster services already started")
+
+    @mock.patch('crmsh.ui_cluster.err_buf.info')
+    @mock.patch('crmsh.utils.start_service')
+    @mock.patch('crmsh.utils.service_is_active')
+    def test_do_start(self, mock_active, mock_start, mock_info):
+        context_inst = mock.Mock()
+        mock_active.return_value = False
+
+        self.ui_cluster_inst.do_start(context_inst)
+
+        mock_active.assert_called_once_with("pacemaker.service")
+        mock_start.assert_called_once_with("pacemaker")
+        mock_info.assert_called_once_with("Cluster services started")
+
+    @mock.patch('crmsh.ui_cluster.err_buf.info')
+    @mock.patch('crmsh.utils.service_is_active')
+    def test_do_stop_already_stopped(self, mock_active, mock_info):
+        context_inst = mock.Mock()
+        mock_active.return_value = False
+        self.ui_cluster_inst.do_stop(context_inst)
+        mock_active.assert_called_once_with("corosync.service")
+        mock_info.assert_called_once_with("Cluster services already stopped")
+
+    @mock.patch('crmsh.ui_cluster.err_buf.info')
+    @mock.patch('crmsh.utils.stop_service')
+    @mock.patch('crmsh.utils.service_is_active')
+    def test_do_stop(self, mock_active, mock_stop, mock_info):
+        context_inst = mock.Mock()
+        mock_active.return_value = True
+
+        self.ui_cluster_inst.do_stop(context_inst)
+
+        mock_active.assert_called_once_with("corosync.service")
+        mock_stop.assert_called_once_with("corosync")
+        mock_info.assert_called_once_with("Cluster services stopped")
