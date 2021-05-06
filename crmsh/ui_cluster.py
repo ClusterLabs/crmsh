139411713169dec624d43b76b2e887a286ddef55
liangxin1300 <XLiang@suse.com>
2021-05-06 09:43:25 +0800

    Fix: bootstrap: change StrictHostKeyChecking=no as a constants(bsc#1185437)

--- crmsh/ui_cluster.py
+++ crmsh/ui_cluster.py
@@ -244,7 +244,7 @@ Note:
         network_group.add_argument("-I", "--ipv6", action="store_true", dest="ipv6",
                                    help="Configure corosync use IPv6")
 
-        qdevice_group = parser.add_argument_group("QDevice configuration", re.sub('  ', '', constants.qdevice_help_info) + "\n\nOptions for configuring QDevice and QNetd.")
+        qdevice_group = parser.add_argument_group("QDevice configuration", re.sub('  ', '', constants.QDEVICE_HELP_INFO) + "\n\nOptions for configuring QDevice and QNetd.")
         qdevice_group.add_argument("--qnetd-hostname", dest="qnetd_addr", metavar="HOST",
                                    help="HOST or IP of the QNetd server to be used")
         qdevice_group.add_argument("--qdevice-port", dest="qdevice_port", metavar="PORT", type=int, default=5403,
