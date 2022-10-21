06761c9661f2b8f7a40811286dfafda3199862ab
nicholasyang <nicholas.yang@suse.com>
2022-10-21 15:13:51 +0800

    Dev: upgradeutil: automated init ssh passwordless auth for hacluster after upgrading (bsc#1201785)
    * add a mechanism for updating cluster config after version update:
        * A sequence is written to ~hacluster/crmsh/upgrade_seq and crmsh checks it to determine whether upgrade is needed whenever it runs.
        * Upgrade is performed only when all nodes in cluster are running compatible version of crmsh. It is determined by comparing CURRENT_UPGRADE_SEQ in upgradeutil.py.
        * Upgrade needs user confirm and is only performed in interactive mode.
    * setup passwordless ssh authentication for user hacluster with that mechanism.

--- crmsh/bootstrap.py
+++ crmsh/bootstrap.py
@@ -25,6 +25,7 @@ from lxml import etree
 from pathlib import Path
 from contextlib import contextmanager
 from . import config
+from . import upgradeutil
 from . import utils
 from . import xmlutil
 from .cibconfig import mkset_obj, cib_factory
@@ -1251,6 +1252,10 @@ def init_sbd():
     _context.sbd_manager.sbd_init()
 
 
+def init_upgradeutil():
+    upgradeutil.force_set_local_upgrade_seq()
+
+
 def init_ocfs2():
     """
     OCFS2 configure process
@@ -2049,6 +2054,7 @@ def bootstrap_init(context):
         init_corosync()
         init_remote_auth()
         init_sbd()
+        init_upgradeutil()
 
         lock_inst = lock.Lock()
         try:
@@ -2120,6 +2126,7 @@ def bootstrap_join(context):
             cluster_node = prompt_for_string("IP address or hostname of existing node (e.g.: 192.168.1.1)", ".+")
             _context.cluster_node = cluster_node
 
+        init_upgradeutil()
         utils.ping_node(cluster_node)
 
         join_ssh(cluster_node)
