ClusterLabs/crmsh

commit 7629864d3db96dc97ad1fc9a9c4e14e5a201b2c9

Fix: help: show help message from argparse(bsc#1175982)

crmsh/help.py
@@ -27,7 +27,7 @@ Help for the level itself is like this:
 
 import os
 import re
-from .utils import page_string
+from .utils import page_string, get_stdout_stderr
 from .msg import common_err
 from . import config
 from . import clidisplay
@@ -100,6 +100,9 @@ class HelpEntry(object):
             prefix = helpfilter("(Redirected from `%s` to `%s`)\n" % self.alias_for)
 
         page_string(short_help + '\n' + prefix + long_help)
+
+    def set_long_help(self, long_help):
+        self.long = long_help
 
     def __str__(self):
         if self.long:
@@ -300,9 +303,15 @@ def _load_help():
 
     def parse_header(line):
         'returns a new entry'
-        entry = {'type': '', 'name': '', 'short': '', 'long': ''}
+        entry = {'type': '', 'name': '', 'short': '', 'long': '', "from_cli": False}
         line = line[2:-3]  # strip [[ and ]]\n
         info, short_help = line.split(',', 1)
+        # TODO see https://github.com/ClusterLabs/crmsh/pull/644
+        # This solution has shortcome to delete the content of adoc,
+        # which lose the static man page archive
+        if "From Code" in short_help:
+            short_help, _ = short_help.split(',')
+            entry['from_cli'] = True
         entry['short'] = short_help.strip()
         info = info.split('_')
         if info[0] == 'topics':
@@ -335,6 +344,10 @@ def _load_help():
             lvl = entry['level']
             if lvl not in _COMMANDS:
                 _COMMANDS[lvl] = odict()
+            if entry['from_cli']:
+                _, help_output, _ = get_stdout_stderr("crm {} {} --help".format(lvl, name))
+                if help_output:
+                    helpobj.set_long_help(help_output.rstrip())
             _COMMANDS[lvl][name] = helpobj
 
     def filter_line(line):

crmsh/ui_cluster.py
@@ -187,7 +187,11 @@ class Cluster(command.UI):
         if len(args) > 0:
             if '--dry-run' in args or looks_like_hostnames(args):
                 args = ['--yes', '--nodes'] + [arg for arg in args if arg != '--dry-run']
-        parser = ArgParser(usage="init [options] [STAGE]", epilog="""
+        parser = ArgParser(description="""
+Initialize a cluster from scratch. This command configures
+a complete cluster, and can also add additional cluster
+nodes to the initial one-node cluster using the --nodes
+option.""", usage="init [options] [STAGE]", epilog="""
 
 Stage can be one of:
     ssh         Create SSH keys for passwordless SSH between cluster nodes
@@ -312,7 +316,11 @@ Note:
         '''
         Join this node to an existing cluster
         '''
-        parser = ArgParser(usage="join [options] [STAGE]", epilog="""
+        parser = ArgParser(description="""
+Join the current node to an existing cluster. The
+current node cannot be a member of a cluster already.
+Pass any node in the existing cluster as the argument
+to the -c option.""",usage="join [options] [STAGE]", epilog="""
 
 Stage can be one of:
     ssh         Obtain SSH keys from existing cluster node (requires -c <host>)
@@ -368,7 +376,10 @@ If stage is not specified, each stage will be invoked in sequence.
         Installs packages, sets up corosync and pacemaker, etc.
         Must be executed from a node in the existing cluster.
         '''
-        parser = ArgParser(usage="add [options] [node ...]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
+        parser = ArgParser(description="""
+Add a new node to the cluster. The new node will be
+configured as a cluster member.""",
+                usage="add [options] [node ...]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("-h", "--help", action="store_true", dest="help", help="Show this help message")
         parser.add_argument("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")
         options, args = parse_options(parser, args)
@@ -386,7 +397,14 @@ If stage is not specified, each stage will be invoked in sequence.
         '''
         Remove the given node(s) from the cluster.
         '''
-        parser = ArgParser(usage="remove [options] [<node> ...]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
+        parser = ArgParser(description="""
+Remove one or more nodes from the cluster.
+
+This command can remove the last node in the cluster,
+thus effectively removing the whole cluster. To remove
+the last node, pass --force argument to crm or set
+the config.core.force option.""",
+                usage="remove [options] [<node> ...]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("-h", "--help", action="store_true", dest="help", help="Show this help message")
         parser.add_argument("-q", "--quiet", help="Be quiet (don't describe what's happening, just do it)", action="store_true", dest="quiet")
         parser.add_argument("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")
@@ -465,7 +483,13 @@ If stage is not specified, each stage will be invoked in sequence.
         * arbitrator IP / hostname (optional)
         * list of tickets (can be empty)
         '''
-        parser = ArgParser(usage="geo-init [options]", epilog="""
+        parser = ArgParser(description="""
+Create a new geo cluster with the current cluster as the
+first member. Pass the complete geo cluster topology as
+arguments to this command, and then use geo-join and
+geo-init-arbitrator to add the remaining members to
+the geo cluster.""",
+        usage="geo-init [options]", epilog="""
 
 Cluster Description
 
@@ -522,7 +546,17 @@ Cluster Description
         '''
         Join this cluster to a geo configuration.
         '''
-        parser = ArgParser(usage="geo-join [options]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
+        parser = ArgParser(description="""
+This command should be run from one of the nodes in a cluster
+which is currently not a member of a geo cluster. The geo
+cluster configuration will be fetched from the provided node,
+and the cluster will be added to the geo cluster.
+
+Note that each cluster in a geo cluster needs to have a unique
+name set. The cluster name can be set using the --name argument
+to init, or by configuring corosync with the cluster name in
+an existing cluster.""",
+                usage="geo-join [options]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("-h", "--help", action="store_true", dest="help", help="Show this help message")
         parser.add_argument("-q", "--quiet", help="Be quiet (don't describe what's happening, just do it)", action="store_true", dest="quiet")
         parser.add_argument("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")
@@ -556,7 +590,11 @@ Cluster Description
         '''
         Make this node a geo arbitrator.
         '''
-        parser = ArgParser(usage="geo-init-arbitrator [options]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
+        parser = ArgParser(description="""
+Configure the current node as a geo arbitrator. The command
+requires an existing geo cluster or geo arbitrator from which
+to get the geo cluster configuration.""",
+                usage="geo-init-arbitrator [options]", add_help=False, formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("-h", "--help", action="store_true", dest="help", help="Show this help message")
         parser.add_argument("-q", "--quiet", help="Be quiet (don't describe what's happening, just do it)", action="store_true", dest="quiet")
         parser.add_argument("-y", "--yes", help='Answer "yes" to all prompts (use with caution)', action="store_true", dest="yes_to_all")

crmsh/ui_context.py
@@ -76,7 +76,7 @@ class Context(object):
                 self.command_info = self.current_level().get_child(token)
                 if not self.command_info:
                     self.fatal_error("No such command")
-                if self.command_name in self.command_info.aliases:
+                if self.command_name in self.command_info.aliases and self.command_name not in ["-h", "--help"]:
                     common_warn("This command '{}' is deprecated, please use '{}'"\
                             .format(self.command_name, self.command_info.name))
                 self.command_name = self.command_info.name

commit f5c0169bb08ee8b16aa56f1c19478a8827426e29

Fix: help: Long time to load and parse crm.8.adoc (bsc#1210198)
This issue was introduced from commit 7629864d;
For those commands that were using argparse, they should get the output
when needed, instead of loading each time.

crmsh/help.py
@@ -76,6 +76,9 @@ class HelpEntry(object):
         self.long = long_help
         self.alias_for = alias_for
         self.generated = generated
+        self.from_cli = False
+        self.level = False
+        self.name = False
 
     def is_alias(self):
         return self.alias_for is not None
@@ -88,6 +91,10 @@ class HelpEntry(object):
         helpfilter = HelpFilter()
 
         short_help = clidisplay.help_header(self.short)
+        if self.from_cli and self.level and self.name:
+            _, output, _ = get_stdout_stderr(f"crm {self.level} {self.name} --help")
+            page_string(short_help + '\n\n'+ output)
+            return
 
         long_help = self.long
         if long_help:
@@ -103,6 +110,11 @@ class HelpEntry(object):
 
     def set_long_help(self, long_help):
         self.long = long_help
+
+    def set_long_lazy_load_source(self, level, name, from_cli):
+        self.level = level
+        self.name = name
+        self.from_cli = from_cli
 
     def __str__(self):
         if self.long:
@@ -344,10 +356,7 @@ def _load_help():
             lvl = entry['level']
             if lvl not in _COMMANDS:
                 _COMMANDS[lvl] = odict()
-            if entry['from_cli']:
-                _, help_output, _ = get_stdout_stderr("crm {} {} --help".format(lvl, name))
-                if help_output:
-                    helpobj.set_long_help(help_output.rstrip())
+            helpobj.set_long_lazy_load_source(entry['level'], entry['name'], entry['from_cli'])
             _COMMANDS[lvl][name] = helpobj
 
     def filter_line(line):