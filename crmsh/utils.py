9cc7ed5e5dc5d5047ed616a065e30a5a7c86c7f7
xin liang <xliang@suse.com>
2023-09-25 14:30:14 +0800

    Fix: utils: Add 'sudo' only when there is a sudoer(bsc#1215549)

--- crmsh/utils.py
+++ crmsh/utils.py
@@ -184,7 +184,9 @@ class UserOfHost:
     def _guess_user_for_ssh(host: str) -> typing.Tuple[str, str]:
         args = ['ssh']
         args.extend(constants.SSH_OPTION_ARGS)
-        args.extend(['-o', 'BatchMode=yes', host, 'sudo', 'true'])
+        sudo_str = 'sudo ' if userdir.get_sudoer() else ''
+        cmd_str = f"-o BatchMode=yes {host} {sudo_str}true"
+        args.extend(shlex.split(cmd_str))
         rc = subprocess.call(
             args,
             stdin=subprocess.DEVNULL,
