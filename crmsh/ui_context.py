ClusterLabs/crmsh

commit beb26f3ee68448d9d6c8243ca0ca394106323a11

Dev: ui_context: Skip querying CIB when in a sublevel or help command
When cluster service is not running, entering some crm sublevel requires live cib,
and it will fail to show the help info under this sublevel;
To fix that, should skip querying cib when the current stack is just a sublevel,
or when the current command is the non functional command (like help, ls, cd, up, quit)

crmsh/ui_context.py
@@ -82,6 +82,10 @@ class Context(object):
                     cmd = True
                     break
             if cmd:
+                if self.command_name not in constants.NON_FUNCTIONAL_COMMANDS:
+                    entry = self.current_level()
+                    if 'requires' in dir(entry) and not entry.requires():
+                        self.fatal_error("Missing requirements")
                 utils.check_user_access(self.current_level().name)
                 rv = self.execute_command() is not False
         except (ValueError, IOError) as e:
@@ -236,8 +240,7 @@ class Context(object):
     def enter_level(self, level):
         '''
         Pushes an instance of the given UILevel
-        subclass onto self.stack. Checks prerequirements
-        for the level (if any).
+        subclass onto self.stack.
         '''
         # on entering new level we need to set the
         # interactive option _before_ creating the level
@@ -248,8 +251,6 @@ class Context(object):
         self._in_transit = True
 
         entry = level()
-        if 'requires' in dir(entry) and not entry.requires():
-            self.fatal_error("Missing requirements")
         self.stack.append(entry)
         self.clear_readline_cache()
 
@@ -317,7 +318,8 @@ class Context(object):
         '''
         ok = True
         if len(self.stack) > 1:
-            ok = self.current_level().end_game(no_questions_asked=self._in_transit) is not False
+            if self.command_name and self.command_name not in constants.NON_FUNCTIONAL_COMMANDS:
+                ok = self.current_level().end_game(no_questions_asked=self._in_transit) is not False
             self.stack.pop()
             self.clear_readline_cache()
         return ok
@@ -338,7 +340,9 @@ class Context(object):
         '''
         Exit from the top level
         '''
-        ok = self.current_level().end_game()
+        ok = True
+        if self.command_name and self.command_name not in constants.NON_FUNCTIONAL_COMMANDS:
+            ok = self.current_level().end_game()
         if options.interactive and not options.batch:
             if constants.need_reset:
                 utils.ext_cmd("reset")

crmsh/constants.py
@@ -516,4 +516,5 @@ Allowed value is nic name or IP address.
 If a nic name is provided, the first IP of that nic will be used. 
 Use multiple -i for more links. Note: Only one link is allowed for the non knet transport type
 """
+NON_FUNCTIONAL_COMMANDS = ('help', 'cd', 'ls', 'quit', 'up')
 # vim:ts=4:sw=4:et:

commit ac4a532dbe79815af8abe847becf74e9b87dbd09

Fix: ui_context: crm configure up prompt #1466
The regression was introduced in beb26f3e
Before beb26f3e it was like
$ crm(live/15sp5-1)configure# primitive d Dummy
$ crm(live/15sp5-1)configure# up
$ There are changes pending. Do you want to commit them (y/n)?
After beb26f3e there is no prompt 'There are changes pending... (y/n)?'
This change brings the prompt back.

crmsh/ui_context.py
@@ -326,7 +326,7 @@ class Context(object):
         '''
         ok = True
         if len(self.stack) > 1:
-            if self.command_name and self.command_name not in constants.NON_FUNCTIONAL_COMMANDS:
+            if ServiceManager().service_is_active("pacemaker.service"):
                 ok = self.current_level().end_game(no_questions_asked=self._in_transit) is not False
             self.stack.pop()
             self.clear_readline_cache()