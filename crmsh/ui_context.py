ClusterLabs/crmsh

commit ac812f17b1fd94ff489e1f0ca17e968019a08417

Fix: cli: loading cib should be a soft depenedency
or it will prevent the interactive cli entering a sublevel

crmsh/ui_context.py
@@ -11,7 +11,6 @@ from . import userdir
 from . import constants
 from . import log
 from . import main
-from .service_manager import ServiceManager
 
 
 logger = log.setup_logger(__name__)
@@ -258,9 +257,8 @@ class Context(object):
         self._in_transit = True
 
         entry = level()
-        if ServiceManager().service_is_active("pacemaker.service"):
-            if 'requires' in dir(entry) and not entry.requires():
-                self.fatal_error("Missing requirements")
+        if 'requires' in dir(entry) and not entry.requires():
+            self.fatal_error("Missing requirements")
         self.stack.append(entry)
         self.clear_readline_cache()
 

crmsh/cibconfig.py
@@ -2769,14 +2769,18 @@ class CibFactory(object):
                 # no need to warn, user can see the object displayed as XML
                 logger.debug("object %s cannot be represented in the CLI notation", obj.obj_id)
 
-    def initialize(self, cib=None):
+    def initialize(self, cib=None, no_side_effects=False):
         if self.cib_elem is not None:
             return True
         if cib is None:
-            cib = read_cib(cibdump2elem)
+            cib_element = read_cib(lambda x: cibdump2elem(x, no_side_effects=no_side_effects))
+            if cib_element is None and no_side_effects:
+                return False
         elif isinstance(cib, str):
-            cib = text2elem(cib)
-        if not self._import_cib(cib):
+            cib_element = text2elem(cib)
+        else:
+            cib_element = cib
+        if not self._import_cib(cib_element):
             return False
         self.cib_orig = copy.deepcopy(self.cib_elem)
         sanitize_cib_for_patching(self.cib_orig)

crmsh/ui_assist.py
@@ -29,7 +29,8 @@ class Assist(command.UI):
         command.UI.__init__(self)
 
     def requires(self):
-        return cib_factory.initialize()
+        cib_factory.initialize(no_side_effects=True)
+        return True
 
     @command.skill_level('administrator')
     @command.completers_repeating(compl.call(cib_factory.prim_id_list))

crmsh/ui_configure.py
@@ -512,8 +512,8 @@ class CibConfig(command.UI):
         # immediately so that tab completion works
 
     def requires(self):
-        if not cib_factory.initialize():
-            return False
+        if not cib_factory.initialize(no_side_effects=True):
+            return True
         # see the configure ptest/simulate command
         has_ptest = utils.is_program('ptest')
         has_simulate = utils.is_program('crm_simulate')

crmsh/ui_maintenance.py
@@ -26,7 +26,8 @@ class Maintenance(command.UI):
         command.UI.__init__(self)
 
     def requires(self):
-        return cib_factory.initialize()
+        cib_factory.initialize(no_side_effects=True)
+        return True
 
     def _onoff(self, resource, onoff):
         if resource is not None:

crmsh/xmlutil.py
@@ -134,7 +134,7 @@ def text2elem(text):
         return None
 
 
-def cibdump2elem(section=None):
+def cibdump2elem(section=None, no_side_effects=False):
     if section:
         cmd = "%s -o %s" % (cib_dump, section)
     else:
@@ -142,7 +142,7 @@ def cibdump2elem(section=None):
     rc, outp, errp = sudocall(cmd)
     if rc == 0:
         return text2elem(outp)
-    else:
+    elif not no_side_effects:
         logger.error("running %s: %s", cmd, errp)
     return None
 

commit 99d6430780f3b2307ec45aa06baa447f80324a06

Fix: ui_context: enter_level() should not check requirements for non-functional subcommands
Fix a regression introduced in ac812f17b1fd94ff489e1f0ca17e968019a08417.

crmsh/ui_context.py
@@ -16,7 +16,7 @@ from . import main
 logger = log.setup_logger(__name__)
 logger_utils = log.LoggerUtils(logger)
 
-_NON_FUNCTIONAL_COMMANDS = {'help', 'cd', 'ls', 'quit', 'up'}
+_NON_FUNCTIONAL_COMMANDS = {'help', 'ls'}
 _NON_FUNCTIONAL_OPTIONS = {'--help', '--help-without-redirect'}
 
 class Context(object):
@@ -257,8 +257,10 @@ class Context(object):
         self._in_transit = True
 
         entry = level()
-        if 'requires' in dir(entry) and not entry.requires():
-            self.fatal_error("Missing requirements")
+        if self.command_name not in _NON_FUNCTIONAL_COMMANDS \
+                and all(arg not in _NON_FUNCTIONAL_OPTIONS for arg in self.command_args):
+            if 'requires' in dir(entry) and not entry.requires():
+                self.fatal_error("Missing requirements")
         self.stack.append(entry)
         self.clear_readline_cache()
 