1c09377ed2eca709d42357f963f8410af7c1c20e
nicholasyang <nicholas.yang@suse.com>
2024-09-23 12:35:48 +0800

    Fix: report: should not try interactive authentication when stdin is not a tty (bsc#1228899)

--- crmsh/report/core.py
+++ crmsh/report/core.py
@@ -19,7 +19,7 @@ from typing import List
 import crmsh.sh
 import crmsh.report.sh
 import crmsh.user_of_host
-from crmsh import utils as crmutils
+from crmsh import utils as crmutils, userdir
 from crmsh import constants as crmconstants
 from crmsh import config, log, tmpfiles, ui_cluster
 from crmsh.sh import ShellUtils
@@ -405,6 +405,11 @@ def find_ssh_user(context: Context) -> None:
                 logger.warning('%s', buf.getvalue())
         else:
             logger.warning("passwordless ssh to node %s does not work", n)
+            if not crmutils.can_ask():
+                logger.error('Cannot create a report non-interactively. Interactive authentication is required.')
+                if userdir.getuser() == 'hacluster':
+                    logger.info('Please setup passwordless ssh authentication for user hacluster.')
+                raise ValueError('Cannot create a report.')
 
 
 def load_from_crmsh_config(context: Context) -> None:
