3675d6ef573b2edb5d9dcd451999a78d7da11daf
nicholasyang <nicholas.yang@suse.com>
2024-09-25 15:11:34 +0800

    Fix: report: find_shell should accept hacluster user (bsc#1228899)

--- crmsh/report/sh.py
+++ crmsh/report/sh.py
@@ -43,41 +43,58 @@ class Shell:
         # call can_run_as here to populate know_hosts
         if not ssh_shell.can_run_as(host, user):
             return None
-        # check for root privilege
-        ret = ssh_shell.subprocess_run_without_input(
-                host, user,
-                'true' if user == 'root' else 'sudo true',
-                start_new_session=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-        )
-        if ret.returncode == 0:
-            return Shell(local_shell, host, user)
+        if user == 'root' or user == 'hacluster':
+            return SSHShell(ssh_shell, host, user)
         else:
-            return None
-
-    def __init__(self, local_shell: crmsh.sh.LocalShell, host: str, user: str):
-        self.local_shell = local_shell
-        self._host = host
-        self._user = user
+            # check for root/hacluster privilege
+            ret = ssh_shell.subprocess_run_without_input(
+                    host, user,
+                    'sudo true',
+                    start_new_session=True,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+            )
+            if ret.returncode == 0:
+                return SSHSudoShell(ssh_shell, host, user)
+            else:
+                return None
 
     def subprocess_run_without_input(self, cmd: str, **kwargs):
-        if self._user == self.local_shell.get_effective_user_name():
-            args = ['/bin/sh']
-        else:
-            args = ['sudo', '-H', '-u', self._user, '/bin/sh']
-        return subprocess.run(
-            args,
-            input=cmd.encode('utf-8'),
-            env=os.environ,  # bsc#1205925
-            **kwargs,
-        )
+        raise NotImplementedError
 
 
-class ClusterShellAdaptor:
+class ClusterShellAdaptor(Shell):
     def __init__(self, cluster_shell: crmsh.sh.ClusterShell, host):
         self.cluster_shell = cluster_shell
         self._host = host
 
     def subprocess_run_without_input(self, cmd: str, **kwargs):
         return self.cluster_shell.subprocess_run_without_input(self._host, None, cmd, **kwargs)
+
+
+class SSHShell(Shell):
+    def __init__(self, ssh_shell: crmsh.sh.SSHShell, host, user):
+        self.ssh_shell = ssh_shell
+        self._host = host
+        self._user = user
+
+    def subprocess_run_without_input(self, cmd: str, **kwargs):
+        return self.ssh_shell.subprocess_run_without_input(
+            self._host, self._user,
+            cmd,
+            **kwargs,
+        )
+
+
+class SSHSudoShell(Shell):
+    def __init__(self, ssh_shell: crmsh.sh.SSHShell, host, user):
+        self.ssh_shell = ssh_shell
+        self._host = host
+        self._user = user
+
+    def subprocess_run_without_input(self, cmd: str, **kwargs):
+        return self.ssh_shell.subprocess_run_without_input(
+            self._host, self._user,
+            f'sudo {cmd}',
+            **kwargs,
+        )
