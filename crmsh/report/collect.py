af67bf7d56cf795c9cecdfd08c30e6d9d32888ea
xin liang <xliang@suse.com>
2024-11-28 09:55:53 +0800

    Dev: report: Remove unused function dump_D_process
    which was used by ocfs2, but ocfs2 related code has been removed.

--- crmsh/report/collect.py
+++ crmsh/report/collect.py
@@ -96,28 +96,6 @@ def collect_journal_logs(context: core.Context) -> None:
         logger.debug(f"Dump jounal log for {item} into {utils.real_path(_file)}")
 
 
-def dump_D_process() -> str:
-    """
-    Dump D-state process stack
-    """
-    out_string = ""
-
-    sh_utils_inst = ShellUtils()
-    _, out, _ = sh_utils_inst.get_stdout_stderr("ps aux|awk '$8 ~ /^D/{print $2}'")
-    len_D_process = len(out.split('\n')) if out else 0
-    out_string += f"Dump D-state process stack: {len_D_process}\n"
-    if len_D_process == 0:
-        return out_string
-
-    for pid in out.split('\n'):
-        _, cmd_out, _ = sh_utils_inst.get_stdout_stderr(f"cat /proc/{pid}/comm")
-        out_string += f"pid: {pid}     comm: {cmd_out}\n"
-        _, stack_out, _ = sh_utils_inst.get_stdout_stderr(f"cat /proc/{pid}/stack")
-        out_string += stack_out + "\n\n"
-
-    return out_string
-
-
 def collect_ratraces(context: core.Context) -> None:
     """
     Collect ra trace file from default /var/lib/heartbeat/trace_ra and custom one
