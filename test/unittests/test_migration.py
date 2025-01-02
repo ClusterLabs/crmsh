e850e4344d39cd9cb48d719a4b3def40a4ad0f81
nicholasyang <nicholas.yang@suse.com>
2025-01-02 08:52:06 +0800

    Dev: add pre-migration checks for pacemaker version (jsc#PED-11808)

--- test/unittests/test_migration.py
+++ test/unittests/test_migration.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from unittest import mock
 
@@ -13,3 +14,30 @@ class TestCheckRemovedResourceAgents(unittest.TestCase):
         self.assertIn(cibquery.ResourceAgent('ocf', 'heartbeat', 'IPaddr2'), s)
         self.assertIn(cibquery.ResourceAgent('stonith', None, 'fence_sbd'), s)
         self.assertNotIn(cibquery.ResourceAgent('foo', None, 'bar'), s)
+
+    def test_check_version_range(self):
+        def check_fn(x):
+            migration._check_version_range(
+                self._handler,
+                'foo',
+                (0, 2,),
+                (1,),
+                re.compile(r'^foo\s+(\d+(?:.\d+)*)'),
+                x,
+            )
+        check_fn('foo 0.2')
+        self._handler.handle_problem.assert_not_called()
+        check_fn('foo 0.2.1')
+        self._handler.handle_problem.assert_not_called()
+        check_fn('foo 0.9')
+        self._handler.handle_problem.assert_not_called()
+        check_fn('foo 0.9.99')
+        self._handler.handle_problem.assert_not_called()
+        check_fn('foo 1')
+        self._handler.handle_problem.assert_called()
+        self._handler.handle_problem.reset_mock()
+        check_fn('foo 1.0')
+        self._handler.handle_problem.assert_called()
+        self._handler.handle_problem.reset_mock()
+        check_fn('foo 1.0.0')
+        self._handler.handle_problem.assert_called()
