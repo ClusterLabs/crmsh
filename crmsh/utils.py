3e3d5fe2df6b2652aa8c2d3216550527f0c71b74
xin liang <xliang@suse.com>
2025-02-09 17:19:32 +0800

    Dev: Add cancel option when confirming to commit for the pending changes

--- crmsh/utils.py
+++ crmsh/utils.py
@@ -271,7 +271,7 @@ def can_ask(background_wait=True):
     return can_ask
 
 
-def ask(msg, background_wait=True):
+def ask(msg, background_wait=True, cancel_option=False):
     """Ask for user confirmation.
 
     Parameters:
@@ -288,9 +288,10 @@ def ask(msg, background_wait=True):
     if not can_ask(background_wait):
         return False
 
+    option_str = "y/n" + "/c" if cancel_option else ""
     msg += ' '
     if msg.endswith('? '):
-        msg = msg[:-2] + ' (y/n)? '
+        msg = msg[:-2] + f'  ({option_str})? '
 
     while True:
         try:
@@ -299,6 +300,8 @@ def ask(msg, background_wait=True):
             ans = 'n'
         if ans:
             ans = ans[0].lower()
+            if ans == 'c':
+                raise TerminateSubCommand
             if ans in 'yn':
                 return ans == 'y'
 
