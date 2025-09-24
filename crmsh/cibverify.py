773f0525e5a1c407d510f33f29f72f82c01daf52
xin liang <xliang@suse.com>
2025-09-24 11:34:48 +0800

    Fix: cibverify: If no errors found, treat crm_verify result as success (bsc#1250349)

--- crmsh/cibverify.py
+++ crmsh/cibverify.py
@@ -10,6 +10,7 @@ logger = log.setup_logger(__name__)
 cib_verify = "crm_verify -VV -p"
 VALIDATE_RE = re.compile(r"^Entity: line (\d)+: element (\w+): " +
                          r"Relax-NG validity error : (.+)$")
+RC_MISCONFIGURATION = 78 # from pacemaker include/crm/common/results.h
 
 
 def _prettify(line, indent=0):
@@ -20,8 +21,13 @@ def _prettify(line, indent=0):
 
 
 def verify(cib):
+    found_error = False
     rc, _, stderr = ShellUtils().get_stdout_stderr(cib_verify, cib.encode('utf-8'))
     for i, line in enumerate(line for line in stderr.split('\n') if line):
         indent = 0 if i == 0 else 7
         print(_prettify(line, indent))
+        if re.search(r"\berror\b", line, re.IGNORECASE):
+            found_error = True
+    if rc == RC_MISCONFIGURATION and not found_error:
+        rc = 1
     return rc
