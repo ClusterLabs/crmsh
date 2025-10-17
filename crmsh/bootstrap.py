d98a54b15a0bdc0c08dbeb2a31e6013d00f7eb84
xin liang <xliang@suse.com>
2025-10-17 21:47:41 +0800

    Dev: bootstrap: Avoid circular import by refactoring FILES_TO_SYNC to use get_files_to_sync()
    - Rename FILES_TO_SYNC to STATIC_FILES_TO_SYNC, holding only static references.
    - Move dynamic imports into a new get_files_to_sync() function.
    - Update all references to FILES_TO_SYNC to call get_files_to_sync() instead.
    - This change prevents circular import issues due to dynamic imports at module level.

--- crmsh/bootstrap.py
+++ crmsh/bootstrap.py
@@ -67,10 +67,9 @@ SERVICES_STOP_LIST = ["corosync-qdevice.service", "corosync.service", "hawk.serv
 BOOTH_DIR = "/etc/booth"
 BOOTH_CFG = "/etc/booth/booth.conf"
 BOOTH_AUTH = "/etc/booth/authkey"
-FILES_TO_SYNC = (BOOTH_DIR, corosync.conf(), COROSYNC_AUTH, CSYNC2_CFG, CSYNC2_KEY, "/etc/ctdb/nodes",
+STATIC_FILES_TO_SYNC = (BOOTH_DIR, COROSYNC_AUTH, CSYNC2_CFG, CSYNC2_KEY, "/etc/ctdb/nodes",
         "/etc/drbd.conf", "/etc/drbd.d", "/etc/ha.d/ldirectord.cf", "/etc/lvm/lvm.conf", "/etc/multipath.conf",
-        "/etc/samba/smb.conf", SYSCONFIG_NFS, SYSCONFIG_PCMK, sbd.SBDManager.SYSCONFIG_SBD, PCMK_REMOTE_AUTH, watchdog.Watchdog.WATCHDOG_CFG,
-        PROFILES_FILE, CRM_CFG, sbd.SBDManager.SBD_SYSTEMD_DELAY_START_DIR)
+        "/etc/samba/smb.conf", SYSCONFIG_NFS, SYSCONFIG_PCMK, PCMK_REMOTE_AUTH, PROFILES_FILE, CRM_CFG)
 
 INIT_STAGES_EXTERNAL = ("ssh", "firewalld", "csync2", "corosync", "sbd", "cluster", "ocfs2", "gfs2", "admin", "qdevice")
 INIT_STAGES_INTERNAL = ("qnetd_remote", )
@@ -1094,7 +1093,7 @@ def init_csync2():
         utils.fatal("Can't create csync2 key {}".format(CSYNC2_KEY))
 
     csync2_file_list = ""
-    for f in FILES_TO_SYNC:
+    for f in get_files_to_sync():
         csync2_file_list += "include {};\n".format(f)
 
     host_str = ""
@@ -1906,7 +1905,7 @@ def sync_files_to_disk():
     """
     target_files_str = ""
 
-    for f in FILES_TO_SYNC:
+    for f in get_files_to_sync():
         # check if the file exists on the local node
         if not os.path.isfile(f):
             continue
@@ -2785,7 +2784,7 @@ def retrieve_all_config_files(cluster_node):
     """
     with logger_utils.status_long("Retrieve all config files"):
         cmd = 'cpio -o << EOF\n{}\nEOF\n'.format(
-            '\n'.join((f for f in FILES_TO_SYNC if f != CSYNC2_KEY and f != CSYNC2_CFG))
+            '\n'.join((f for f in get_files_to_sync() if f != CSYNC2_KEY and f != CSYNC2_CFG))
         )
         pipe_outlet, pipe_inlet = os.pipe()
         try:
@@ -2800,7 +2799,7 @@ def retrieve_all_config_files(cluster_node):
         finally:
             os.close(pipe_inlet)
         rc = child.wait()
-        # Some errors may happen here, since all files in FILES_TO_SYNC may not exist.
+        # Some errors may happen here, since all files in get_files_to_sync() may not exist.
         if result is None or result.returncode == 255:
             utils.fatal("Failed to create ssh connect to {}".format(cluster_node))
         if rc != 0:
@@ -2821,4 +2820,11 @@ def restart_cluster():
     logger.info("Restarting cluster service")
     utils.cluster_run_cmd("crm cluster restart")
     wait_for_cluster()
+
+
+def get_files_to_sync():
+    return (
+        (corosync.conf(), sbd.SBDManager.SYSCONFIG_SBD, watchdog.Watchdog.WATCHDOG_CFG,
+         sbd.SBDManager.SBD_SYSTEMD_DELAY_START_DISABLE_DIR) + STATIC_FILES_TO_SYNC
+    )
 # EOF
