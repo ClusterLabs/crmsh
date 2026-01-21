84b3b639cb8dcaf58500c01b82fa49f8460dfca0
nicholasyang <nicholas.yang@suse.com>
2026-01-21 18:01:48 +0800

    Dev: pre-migration: Add check for obsolete SAP ASCS/ERS ENSA1 (jsc#SAPSOL-780)
    This commit introduces a new check in the migration module to detect obsolete SAP ASCS/ERS ENSA1 configurations.
    
    The check identifies setups where:
    
    - A SAPInstance resource with IS_ERS=true is present.
    - Another SAPInstance resource (ASCS) is present.
    - A location rule binds the ASCS SAPInstance to 'rule score > 0: runs_ers_* eq 1'.
    
    This check ensures that users are warned about obsolete configurations that are not supported in newer versions, guiding them to migrate to ENSA2.

--- test/unittests/test_migration.py
+++ test/unittests/test_migration.py
@@ -138,3 +138,66 @@ class TestCheckObsoletedSapAscsErsMount(unittest.TestCase):
             migration._check_obseleted_sap_ascs_ers_mount(self.handler, cib)
             self.handler.handle_problem.assert_called_once()
 
+
+class TestCheckObsoleteSapAscsErsEnsa1(unittest.TestCase):
+    XML_DATA = '''<?xml version="1.0" ?>
+<cib crm_feature_set="3.16.1" validate-with="pacemaker-3.9" epoch="9" num_updates="0" admin_epoch="0" cib-last-written="Fri Sep 26 15:02:50 2025" update-origin="ha-1-1" update-client="cibadmin" update-user="root" have-quorum="1" dc-uuid="1">
+  <configuration>
+    <crm_config>
+      <cluster_property_set id="cib-bootstrap-options">
+        <nvpair id="cib-bootstrap-options-have-watchdog" name="have-watchdog" value="false"/>
+        <nvpair id="cib-bootstrap-options-dc-version" name="dc-version" value="2.1.5+20221208.a3f44794f-150500.4.9-2.1.5+20221208.a3f44794f"/>
+        <nvpair id="cib-bootstrap-options-cluster-infrastructure" name="cluster-infrastructure" value="corosync"/>
+        <nvpair id="cib-bootstrap-options-cluster-name" name="cluster-name" value="hacluster"/>
+        <nvpair name="stonith-enabled" value="false" id="cib-bootstrap-options-stonith-enabled"/>
+      </cluster_property_set>
+    </crm_config>
+    <nodes>
+      <node id="1" uname="ha-1-1"/>
+    </nodes>
+    <resources>
+      <primitive id="ascs_instance" class="ocf" provider="heartbeat" type="SAPInstance" />
+      <primitive id="ers_instance" class="ocf" provider="heartbeat" type="SAPInstance">
+        <instance_attributes id="ers_instance-instance_attributes">
+          <nvpair name="IS_ERS" value="true" id="ers_instance-instance_attributes-IS_ERS"/>
+        </instance_attributes>
+      </primitive>
+    </resources>
+    <constraints>
+        <rsc_location id="loc_failover_to_ers" rsc="ascs_instance">
+            <rule score="1000" id="loc_failover_to_ers-rule">
+                <expression operation="eq" attribute="runs_ers_ers_instance" value="1" id="loc_failover_to_ers-rule-expression"/>
+            </rule>
+        </rsc_location>
+    </constraints>
+  </configuration>
+</cib>
+'''
+
+    def setUp(self):
+        self.handler = mock.Mock(migration.CheckResultHandler)
+        self.cib = lxml.etree.fromstring(self.XML_DATA)
+
+    def test_check_obsolete_sap_ascs_ers_ensa1(self):
+        migration._check_obsolete_sap_ascs_ers_ensa1(self.handler, self.cib)
+        self.handler.handle_problem.assert_called_once_with(
+            False,
+            "SAP ASCS/ERS ENSA1 is obsolete and not supported in SLES 16. Please migrate to ENSA2.",
+            ['* SAPInstance resource "ascs_instance" has a location constraint with a rule matching the ENSA1 pattern (attribute "runs_ers_ers_instance").']
+        )
+
+    def test_score_zero_or_less_not_triggered(self):
+        for score in ("0", "-100"):
+            self.handler.handle_problem.reset_mock()
+            xml_data = self.XML_DATA.replace('score="1000"', f'score="{score}"')
+            cib = lxml.etree.fromstring(xml_data)
+            migration._check_obsolete_sap_ascs_ers_ensa1(self.handler, cib)
+            self.handler.handle_problem.assert_not_called()
+
+    def test_nvpair_boolean_value_case_insensitive(self):
+        for val in ("True", "TRUE", "tRUE"):
+            self.handler.handle_problem.reset_mock()
+            xml_data = self.XML_DATA.replace('value="true"', f'value="{val}"')
+            cib = lxml.etree.fromstring(xml_data)
+            migration._check_obsolete_sap_ascs_ers_ensa1(self.handler, cib)
+            self.handler.handle_problem.assert_called_once()
